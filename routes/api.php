457ee9386cb37acae050435e53fab8cdd0b66f55
arup.das1 <arup.das@anudip.org>
2023-04-14 01:08:15 +0530

    first commit ever in dev

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use App\Http\Controllers\API\UserController;
 |
 */
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
