8d1f86f4fbabe47a5c67e8a2f3ba36fde0e15ff8
arup.das1 <arup.das@anudip.org>
2023-04-18 16:28:52 +0530

    first commit ever in dev

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use Illuminate\Http\Request;
-
+use Carbon\Carbon;
 class AttendanceController extends Controller
 {
     /**
@@ -20,15 +20,48 @@ class AttendanceController extends Controller
      */
     public function create()
     {
-        //
+       
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function storeAttendance(Request $request)
     {
         //
+         //
+         DB::beginTransaction();
+         try { 
+                 $details = Attendance::where('atten_date', Carbon::parse($request->atten_date)->format('Y-m-d'))->get();
+                 if(sizeof($details)>0){
+ 
+                     $option = Attendance::where('atten_date', '=', Carbon::parse($request->atten_date)->format('Y-m-d'))->get();
+                 
+                     $option->lat = $request->lat;
+                     $option->long = $request->long;
+                     $option->punch_out = Carbon::parse($request->atten)->format('Y-m-d');
+                     $option->save();
+                     $this->sendResponse(['message' => 'Updated successfully','status'=>1], 'Updated successfully');
+                 }
+ 
+ 
+             
+ 
+             $data = new Attendance;
+     
+             $data->user_id = $request->user_id;
+             $data->atten_date = Carbon::parse($request->atten)->format('Y-m-d');
+             $data->punch_in = Carbon::parse($request->atten)->format('Y-m-d');
+             $data->punch_out = Carbon::parse($request->atten)->format('Y-m-d');
+             $data->lat = $request->lat;
+             $data->long = $request->long;
+             $data->save();
+             $this->sendResponse(['message' => 'inserted successfully','status'=>1], 'inserted successfully');
+ 
+         } catch (Exception $e) { 
+             DB::rollback();
+             return $this->sendError($e->getMessage());
+     }
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\UserController;
+use App\Http\Controllers\api\AttendanceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,5 @@ Route::post('login',[UserController::class,'loginUser']);
 Route::group(['middleware' => 'auth:sanctum'],function(){
     Route::get('user',[UserController::class,'userDetails']);
     Route::get('logout',[UserController::class,'logout']);
+    Route::post('store-attendance',[AttendanceController::class,'storeAttendance']);
 });
