5ba54cef5abe7c68fb34756add63f1d25fbd3083
arup.das1 <arup.das@anudip.org>
2023-04-18 13:36:52 +0530

    first commit ever in dev

---
+++ database/migrations/2023_04_18_074227_create_attendances_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('atten_date')->nullable();
+            $table->time('punch_id')->nullable();
+            $table->time('punch_out')->nullable();
+            $table->string('lat')->nullable();
+            $table->string('long')->nullable();
+            $table->integer('member_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
