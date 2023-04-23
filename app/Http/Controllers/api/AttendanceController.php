d199834968c0d97f23da665b146ad668e423e5f7
arup.das1 <arup.das@anudip.org>
2023-04-23 12:49:15 +0530

    first commit ever in dev

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -31,47 +31,37 @@ class AttendanceController extends Controller
     public function storeAttendance(Request $request)
     {
         
-         DB::beginTransaction();
-         try { 
-                 date_default_timezone_set('Asia/Calcutta');
-                 $details = Attendance::where('atten_date', date('Y-m-d'))->get();
-                 if(sizeof($details)>0){
- 
-                    Attendance::where('atten_date', date('Y-m-d'))->update(['punch_out'=>date('H:i:s'),'lat'=>$request->lat,'long'=>$request->long]);
-                    $x=['punch_out'=>date('H:i:s'),'atten_date' => date('Y-m-d'),'punch_in'=>$details[0]->punch_in];
-                    DB::commit();
-                     return Response(['message' => 'updated successfully','status'=>1,'data'=>$x],200);
-                 }
-                 if(str_starts_with($request->member_code, 'AF')){
-                   $member_type='student';
-                 }else{
-                    $member_type='staff';
-                 }
-                 
-                 $attn_type='present';
-                 Attendance::create(['user_id' => $request->user_id,'atten_date' => date('Y-m-d'),'punch_in'=>date('H:i:s'),'lat'=>$request->lat,'long'=>$request->long,'member_id'=>$request->member_id,'member_code'=>$request->member_code,'status'=>0,'transfer_status'=>0,'attn_type'=>$attn_type,'member_type'=>$member_type]);
+        DB::beginTransaction();
+        try { 
+            date_default_timezone_set('Asia/Calcutta');
+            $details = Attendance::where('atten_date', $request->atten_date)->get();
+            if(sizeof($details)>0){
+            Attendance::where('atten_date', $request->atten_date)->update(['punch_out'=>date('H:i:s'),'lat'=>$request->lat,'long'=>$request->long]);
+            $x=['punch_out'=>date('H:i:s'),'atten_date' => $request->atten_date,'punch_in'=>$details[0]->punch_in];
+            DB::commit();
+                return Response(['message' => 'updated successfully','status'=>1,'data'=>$x],200);
+            }
+            if(str_starts_with($request->member_code, 'AF')){
+            $member_type='student';
+            }else{
+            $member_type='staff';
+            }
+            $attn_type='present';
+            $postParameter = ['user_id' => $request->user_id,'atten_date' => $request->atten_date,'punch_in'=>date('H:i:s'),'lat'=>$request->lat,'long'=>$request->long,'member_id'=>$request->member_id,'member_code'=>$request->member_code,'status'=>0,'transfer_status'=>0,'attn_type'=>$attn_type,'member_type'=>$member_type];
+            Attendance::create($postParameter);
+            $curlHandle = curl_init('https://cmis3api.anudip.org/api/insertFromAttenApp');
+            curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
+            curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+            $curlResponse = curl_exec($curlHandle);
+            curl_close($curlHandle);
+            $x=['punch_in'=>date('H:i:s'),'atten_date' => date('Y-m-d')];
+            DB::commit();
+            return Response(['message' => 'inserted successfully','status'=>1,'data'=>$x],200);
 
-
-                 $postParameter = ['user_id' => $request->user_id,'atten_date' => date('Y-m-d'),'punch_in'=>date('H:i:s'),'lat'=>$request->lat,'long'=>$request->long,'member_id'=>$request->member_id,'member_code'=>$request->member_code,'status'=>0,'transfer_status'=>0,'attn_type'=>$attn_type,'member_type'=>$member_type];
-                
-                $curlHandle = curl_init('https://cmis3api.anudip.org/api/insertFromAttenApp');
-                curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
-                curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-                
-                $curlResponse = curl_exec($curlHandle);
-                //dd($curlResponse);
-
-                curl_close($curlHandle);
-             
-                // $this->sendResponse(['message' => 'inserted successfully','status'=>1], 'inserted successfully');
-                $x=['punch_in'=>date('H:i:s'),'atten_date' => date('Y-m-d')];
-                DB::commit();
-                return Response(['message' => 'inserted successfully','status'=>1,'data'=>$x],200);
- 
-         } catch (Exception $e) { 
-             DB::rollback();
-             return $this->sendError($e->getMessage());
-         }
+        } catch (Exception $e) { 
+            DB::rollback();
+            return $this->sendError($e->getMessage());
+        }
     }
 
     /**
