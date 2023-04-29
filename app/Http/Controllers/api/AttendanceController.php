45ae73caea9bb2fd657076928e787d1057c508da
arup.das1 <arup.das@anudip.org>
2023-04-29 18:51:28 +0530

    first commit on git hub

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -144,9 +144,9 @@ class AttendanceController extends Controller
     public function insertIntoAttendanceFromCMIS(Request $request){
        
         //return $this->continue_func(56);
-        return Response(['data' => $request->all()],200);
+        //return Response(['data' => $request->all()],200);
         $x=[];
-        foreach(json_decode($request->all()) as $r){
+        foreach($request->all() as $r){
             array_push($x,$r);
             
         }
