a065864621419cbc9cacdfb973c4290ec54ee96a
arup.das1 <arup.das@anudip.org>
2023-05-08 11:38:33 +0530

    first commit on git hub

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -86,7 +86,7 @@ class AttendanceController extends Controller
             $attn_date=date('Y-m-d');
         }
         
-        $details = Attendance::where('user_id',$user_id)->where('atten_date', date('Y-m-d'))
+        $details = Attendance::where('user_id',$user_id)->where('atten_date', $attn_date)
         ->get(['id as id','punch_in as punch_in','punch_out as punch_out','atten_date as date','status as status','user_id as user_id']);
         return Response(['datas' => $details,'status'=>1,'cur_date'=>$attn_date],200);
     }
