ad6047e4ea97e00376283f8cf8be642ade82e67a
arup.das1 <arup.das@anudip.org>
2024-05-27 13:13:11 +0530

    check

--- app/Http/Controllers/api/TrainerController.php
+++ app/Http/Controllers/api/TrainerController.php
@@ -143,7 +143,7 @@ class TrainerController extends Controller
                 $input['file']='NA'; 
             }    
 
-            $postParameter = ['user_id' => $request->user_id,'atten_date' => $request->attend_date,'punch_in'=>$time,'lat'=>$request->lat,'long'=>$request->long,'member_id'=>$request->member_id,'member_code'=>$request->member_code,'status'=>2,'transfer_status'=>1,'atten_type'=>$attn_type,'member_type'=>$member_type,'punch_in_place'=>$request->location,'reason'=>$request->reason,'center_id'=>$request->center_id,'photo'=>$input['file'],'batch_id'=>$request->batch_id,'batch_code'=>$request->batch_code,'bulk'=>0];
+            $postParameter = ['user_id' => $request->user_id,'atten_date' => $request->attend_date,'punch_in'=>$time,'lat'=>$request->lat,'long'=>$request->long,'member_id'=>$request->member_id,'member_code'=>$request->member_code,'status'=>2,'transfer_status'=>1,'bulk_type'=>0,'atten_type'=>$attn_type,'member_type'=>$member_type,'punch_in_place'=>$request->location,'reason'=>$request->reason,'center_id'=>$request->center_id,'photo'=>$input['file'],'batch_id'=>$request->batch_id,'batch_code'=>$request->batch_code];
             if(sizeof($details)>0){
                 //dd($details[0]->id);
                 $curlHandle = curl_init('https://cmis4api.anudip.org/public/api/insertFromAttenApp');
