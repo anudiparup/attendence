0b4d39b6e45237bef61d03fdf8cc581baeb0e062
arup.das1 <arup.das@anudip.org>
2024-10-15 19:45:16 +0530

    bulk type add

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -175,7 +175,8 @@ class AttendanceController extends Controller
                 
                 
                 // Optionally, remove the local temporary file
-                unlink($tempPath);
+                //unlink($tempPath);
+                //unlink($tempPath);
             }else{
                 $input['file']='NA'; 
             }    
